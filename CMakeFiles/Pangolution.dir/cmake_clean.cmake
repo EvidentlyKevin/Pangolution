ed3ac2fb5760f535e81c9209c6c5761e2d949756
Kevin Hack <bevinquack@duck.com>
2024-05-29 20:55:41 -0400

    Big Cahuna

---
+++ CMakeFiles/Pangolution.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pangolution.dir/Pangolin.cpp.o"
+  "CMakeFiles/Pangolution.dir/Pangolin.cpp.o.d"
+  "CMakeFiles/Pangolution.dir/Utility.cpp.o"
+  "CMakeFiles/Pangolution.dir/Utility.cpp.o.d"
+  "CMakeFiles/Pangolution.dir/main.cpp.o"
+  "CMakeFiles/Pangolution.dir/main.cpp.o.d"
+  "Pangolution"
+  "Pangolution.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pangolution.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
